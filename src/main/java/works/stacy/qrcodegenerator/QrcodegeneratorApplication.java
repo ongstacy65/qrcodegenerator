1067c8920099df0b45447573e1ce7381e63f663d
Stacy Ong <ongstacy26@gmail.com>
2023-06-11 12:21:22 +0800

    Initial commit

---
+++ src/main/java/works/stacy/qrcodegenerator/QrcodegeneratorApplication.java
@@ -0,0 +1,13 @@
+package works.stacy.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodegeneratorApplication.class, args);
+	}
+
+}
